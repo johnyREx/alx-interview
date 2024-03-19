282bd22b9d0f7e11138059c745aa003193afdee2
johnyREx <johnkwofie99@icloud.com>
2024-03-19 13:11:53 +0300

    pep 8 style fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,15 +3,16 @@
 Module containing minOperations function
 """
 
+
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n H characters
+    Calculates the fewest number of operations needed
 
     Args:
-        n (int): The desired number of H characters
+        n (int):
 
     Returns:
-        int: The fewest number of operations needed, or 0 if n is impossible to achieve
+        int: The fewest number of operations needed, or 0
     """
     if n <= 1:
         return 0
@@ -19,13 +20,13 @@ def minOperations(n):
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return i + minOperations(n // i)
-    
+
     return n
 
 
 if __name__ == "__main__":
     n = 4
-    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+    print("Min number to reach {} characters: {}".format(n, minOperations(n)))
 
     n = 12
-    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+    print("Min number to reach {} characters: {}".format(n, minOperations(n)))
