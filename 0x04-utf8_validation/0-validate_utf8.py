388570c3b5b2278fdefc4fe24f143d49aa8bc4e2
johnyREx <johnkwofie99@icloud.com>
2024-04-04 18:33:32 +0300

    see that girl she looking for a man

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,43 +1,39 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
 def validUTF8(data):
-    # Helper function
-    def is_valid_utf8_char(bytes_sequence):
-        # check if the byte sequence starts with correct prefix
-        if bytes_sequence[0] & 0b10000000 == 0:
-            return True
+    """Determines if a given data set
+    represents a valid utf-8 encoding
+    """
+    number_bytes = 0
 
-        num_bytes = 0
-        for byte in bytes_sequence:
-            if byte & 0b10000000 == 0b10000000:
-                num_bytes += 1
-            else:
-                break
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-            if num_bytes == 1 or num_bytes > 4:
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
 
-            for i in range(1, num_bytes):
-                if (bytes_sequence[i] & 0b11000000) != 0b10000000:
-                    return False
-
-            return True
-
-            i = 0
-            while i < len(data):
-
-                first_byte = data[i]
-
-                if first-byte & 0b10000000 == 0:
-                    i += 1
-
-                else:
-                    num_bytes = 0
-            while first_byte & (0b10000000 >> num_bytes):
-                num_bytes += 1
-
-            # Extract the byte sequence
-            byte_sequence = data[i:i + num_bytes]
-            if not is_valid_utf8_char(byte_sequence):
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
                 return False
-            i += num_bytes
 
-    return True
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
