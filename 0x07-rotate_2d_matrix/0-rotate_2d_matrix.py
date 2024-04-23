4e4793d4d51bf054962965921c919e74bf6926f6
johnyREx <johnkwofie99@icloud.com>
2024-04-23 17:13:11 +0200

    fixed pep8 but i have a feeeling the checker will ruin me

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,11 +6,11 @@
 def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place.
     """
-    if type(matrix) != list:
+    if not isinstance(matrix, list):
         return
     if len(matrix) <= 0:
         return
-    if not all(map(lambda x: type(x) == list, matrix)):
+    if not all(isinstance(x, list) fir x in matrix):
         return
     rows = len(matrix)
     cols = len(matrix[0])
