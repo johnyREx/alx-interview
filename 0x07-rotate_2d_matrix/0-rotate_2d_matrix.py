f4e21c2835c130909219413b6cb97233e9f246e7
johnyREx <johnkwofie99@icloud.com>
2024-04-24 16:22:45 +0200

    i got commitment issues

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,11 +6,11 @@
 def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place.
     """
-    if not isinstance(matrix, list):
+    if type(matrix) != list:
         return
     if len(matrix) <= 0:
         return
-    if not all(isinstance(x, list) fir x in matrix):
+    if not all(map(lambda x: type(x) == list, matrix)):
         return
     rows = len(matrix)
     cols = len(matrix[0])
