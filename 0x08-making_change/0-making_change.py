24693eb776545dfd3574a8d7a2e8fb509f322aea
johnyREx <johnkwofie99@icloud.com>
2024-06-04 09:32:17 +0200

    Change comes from within but i had to go out my way to change this code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Change comes from with"""
+"""Change making module.
+"""
 
 
 def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
-    makeChange: Change comes from within
-    Args:
-    coins: the coins.
-    total: the total amount of coins
-    """
-
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
